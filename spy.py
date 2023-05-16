33b094c163b51dab7b288746d79883ec8d46c079
Soren Althoff <sorenalthoff@gmail.com>
2023-05-16 03:29:18 -0700

    yessir

---
+++ spy.py
@@ -0,0 +1,5 @@
+#1. prompt the user "Enter your first name"
+#2. prompt the user "Enter your last name"
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+print(f'My name is {last_name}, {first_name} {last_name}')
